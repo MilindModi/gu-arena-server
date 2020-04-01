f613fbdf20d2cd48558b2d70b0d8945056ee9afb
milind <milindmodi99@gmail.com>
2020-04-02 02:25:53 +0530

    changes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/get-result-page', get_result_pages);
-
+app.get('/',  function (req, res) {
+    res.send("hello");
+});
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on Port ${port}...`));
