a03cb23682e9b7cf0f290f2d35d2226fa3cb9261
milind <milindmodi99@gmail.com>
2020-04-06 21:51:23 +0530

    new api

--- routes/get-result-page.js
+++ routes/get-result-page.js
@@ -3,7 +3,7 @@ const Jimp = require('jimp');
 const request = require("request");
 const router = express.Router();
 const cheerio = require('cheerio');
-
+const fs = require('fs');
 router.get('/', function (req, res) {
     console.log('URL / : \n');
     url = 'http://result.gujaratuniversity.ac.in/';
@@ -44,5 +44,46 @@ router.get('/', function (req, res) {
       }
     })
   });
-
+  router.get('/new', function (req, res) {
+    console.log('URL / : \n');
+    url = 'http://result.gujaratuniversity.ac.in/';
+    res. setHeader("Content-Type", "application/json");
+    request(url, function(error, response, html){
+      if(!error){
+        var data = {}
+          var $ = cheerio.load(html);
+          var img = $('img.control-label');
+          var img_url = $('img.control-label').attr('src')
+          console.log(img);
+          console.log(url+img_url);
+          console.log(response.headers);
+          var coursesHTML = $('option');  
+          var courseArr = {};
+          $('#exam').find('option').each((i,op) => {
+                console.log($(op).text(),$(op).val())
+                courseArr[$(op).val().toString()] = $(op).text();
+            })  
+            console.log("TEST : "+courseArr['85'] );
+          Jimp.read({
+                      url: url+img_url, // Required!
+                      headers: {Cookie: response.headers},
+                    })
+                      .then(image => {
+                        // Do stuff with the image.' 
+                        image.write("path.jpeg");
+                        data['course'] = courseArr;
+                        data['header'] = response.header;
+                        fs.readFile("./path.jpeg", (err, image1) => {
+                          data['img'] = image1;
+                          res.send(data);  ;
+                          });
+                        
+                     
+                      })
+                      .catch(err => {
+                        console.log(err);
+                      });
+      }
+    })
+  });
 module.exports = router;
